9fe87cc9f04af64e704f4ed9f4cfa910f7453a1a
Evan Assmus <evanwassmus@gmail.com>
2025-11-22 18:10:07 -0500

    init

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,116 @@
+return {
+    "neovim/nvim-lspconfig",
+    version = "*", -- remove after they fix ESLint
+    dependencies = {"hrsh7th/cmp-nvim-lsp", "hrsh7th/nvim-cmp"},
+
+    config = function()
+        local lspconfig_defaults = require('lspconfig').util.default_config
+        lspconfig_defaults.capabilities =
+            vim.tbl_deep_extend('force', lspconfig_defaults.capabilities,
+                                require('cmp_nvim_lsp').default_capabilities())
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            desc = 'LSP actions',
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>',
+                               opts)
+                vim.keymap.set('n', 'gd',
+                               '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD',
+                               '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi',
+                               '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go',
+                               '<cmd>lua vim.lsp.buf.type_definition()<cr>',
+                               opts)
+                vim.keymap.set('n', 'gr',
+                               '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs',
+                               '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>',
+                               opts)
+                vim.keymap.set({'n', 'x'}, '<F3>',
+                               '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+                               opts)
+                vim.keymap.set('n', '<F4>',
+                               '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+            end
+        })
+
+        -----------
+        -- SERVERS
+        require'lspconfig'.html.setup {}
+        require'lspconfig'.cssls.setup {}
+        require'lspconfig'.jsonls.setup {}
+        require'lspconfig'.tailwindcss.setup {}
+        require'lspconfig'.ts_ls.setup {}
+        require'lspconfig'.rust_analyzer.setup {}
+        vim.lsp.config('lua_ls', {
+            cmd = {'lua-language-server'}, -- Or the path to your lua_ls executable
+            filetypes = {'lua'},
+            root_dir = vim.fs.dirname(vim.fs.find({'.git', '.luarc.json'},
+                                                  {upward = true})[1]),
+            settings = {
+                Lua = {
+                    telemetry = {enable = false},
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file('', true) -- Include Neovim's runtime files for completion
+                    }
+                }
+            }
+        })
+
+        vim.lsp.enable('lua_ls')
+        require'lspconfig'.basedpyright.setup {
+            settings = {
+                basedpyright = {analysis = {diagnosticSeverityOverrides = {}}}
+            }
+        }
+        require'lspconfig'.eslint.setup {
+            on_attach = function(client, bufnr)
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    buffer = bufnr,
+                    command = "EslintFixAll"
+                })
+            end
+        }
+        -----------
+
+        local cmp = require('cmp')
+
+        cmp.setup({
+            sources = {{name = "nvim_lsp"}, {name = "luasnip"}},
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                    -- vim.snippet.expand(args.body)
+                end
+            },
+            preselect = 'item',
+            completion = {completeopt = 'menu,menuone,noinsert'},
+            mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+                -- Jump to the next snippet placeholder
+                ['<C-f>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+                    if luasnip.locally_jumpable(1) then
+                        luasnip.jump(1)
+                    else
+                        fallback()
+                    end
+                end, {'i', 's'}),
+                -- Jump to the previous snippet placeholder
+                ['<C-b>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+                    if luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, {'i', 's'})
+            })
+        })
+    end
+}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,62 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = {enabled = true},
+        dashboard = {
+            enabled = true,
+            preset = {
+                ---@type snacks.dashboard.Item[]
+                keys = {
+                    {
+                        icon = " ",
+                        key = "f",
+                        desc = "Find File",
+                        action = ":lua Snacks.dashboard.pick('files')"
+                    },
+                    {
+                        icon = " ",
+                        key = "n",
+                        desc = "New File",
+                        action = ":ene | startinsert"
+                    }, {
+                        icon = " ",
+                        key = "g",
+                        desc = "Find Text",
+                        action = ":lua Snacks.dashboard.pick('live_grep')"
+                    }, {
+                        icon = " ",
+                        key = "r",
+                        desc = "Recent Files",
+                        action = ":lua Snacks.dashboard.pick('oldfiles')"
+                    },
+                    {
+                        icon = " ",
+                        key = "s",
+                        desc = "Restore Session",
+                        section = "session"
+                    }, {icon = " ", key = "q", desc = "Quit", action = ":qa"}
+                },
+                header = [[  ]]
+            },
+            sections = {
+                {section = "header"}, {section = "keys", gap = 1, padding = 1}
+            }
+        },
+        explorer = {enabled = false},
+        indent = {enabled = true},
+        input = {enabled = true},
+        picker = {enabled = false},
+        notifier = {enabled = false},
+        quickfile = {enabled = true},
+        scope = {enabled = true},
+        scroll = {enabled = true},
+        statuscolumn = {enabled = true},
+        words = {enabled = true}
+    }
+}
